937f763601e821ebee4cdebe05e71885d7e9688c
Melike Külahcı <m.edakulahci@gmail.com>
2024-06-25 13:55:03 +0300

    Add files via upload

---
+++ Piton.py
@@ -0,0 +1,21 @@
+print("Hello Python World!")
+print('How are you?')
+
+x=5
+y="Hİ!"
+print(x)
+print(y)
+
+print(2+2)
+print(8-3)
+print(10/4)
+print(10//4)
+print(10%4)
+print(2**2)
+
+"""
+this
+is 
+an 
+apple
+"""
