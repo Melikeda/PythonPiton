1dbc0e962324e395c3cb676e1b8ff14459436642
Melike Külahcı <m.edakulahci@gmail.com>
2024-06-25 14:36:06 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,30 @@
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+
+m = 5
+k = "Melike"
+
+print(m)
+print(type(m))
+print(k)
+print(type(k))
+
+
+fruits = ["apple", "banana", "cherry"]
+a, B, c = fruits
+
+print(a)
+print(B)
+print(c)
+
+namE= "EDA"
+int = 5
+
+print(namE, int)
+
