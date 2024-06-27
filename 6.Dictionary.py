aa00bb7962d3e4eaadff693a25daf374a33d0103
Melike Külahcı <m.edakulahci@gmail.com>
2024-06-27 17:40:21 +0300

    Add files via upload

---
+++ 6.Dictionary.py
@@ -0,0 +1,11 @@
+user = {
+    'melikekulahci':{
+        'age' : 26,
+        'email' : "melikegmail.com",
+        'address' : "california",
+        'phone' : 5078612578,
+
+    }
+}
+
+print(user['melikekulahci']['address'])
