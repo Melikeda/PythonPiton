4549ae7788300bab9ef468d4236bbdeefc7f3b93
Melike Külahcı <m.edakulahci@gmail.com>
2024-06-28 15:03:04 +0300

    Add files via upload

---
+++ 92.İfElifElse.py
@@ -0,0 +1,25 @@
+username = "FestinaLente"
+password = '12345'
+
+if(username == 'FestinaLente'):
+    if(password == '12345'):
+        print("Welcome")
+    else:
+        print("Password is wrong")    
+        
+else:
+    print("Username is wrong")        
+
+
+#####
+
+num = int(input('number:  '))
+
+if num > 0:
+    print("Number is pozitive")
+
+elif num < 0: 
+    print('Number is negative')
+
+else: 
+    print("Number is zero")      
