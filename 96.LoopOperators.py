ecd1b3884d367943f0aef398ca9d1bde011051d6
Melike Külahcı <m.edakulahci@gmail.com>
2024-06-29 12:53:06 +0300

    Add files via upload

---
+++ 96.LoopOperators.py
@@ -0,0 +1,28 @@
+# range
+
+for x in range(1, 11):
+    print(x)
+
+
+for a in range(0, 100, 10):
+    print(a)
+    
+
+for b in range(0, 10, 2):
+    print(list(range(0, 10, 2)))  
+
+
+# zip
+
+
+list1 = ["Hakan", "Emre", "Naz"]
+list2 = [5505, 9999, 7276]
+
+print(list(zip(list1, list2)))
+
+
+
+
+
+
+
