e1c1772d3a3fea8a1a956951309ee0501f2cee0a
Melike Külahcı <m.edakulahci@gmail.com>
2024-06-29 12:07:57 +0300

    Add files via upload

---
+++ 94.WhileLoops.py
@@ -0,0 +1,23 @@
+#1-100
+x = 1
+
+while x < 100:
+    print(x)
+    x += 1
+
+ #1-100 even
+x = 1
+
+while x < 100:
+    if x % 2 == 0:
+     print(x)
+    x += 1
+
+
+ #1-100 odd    
+x = 1
+
+while x < 100:
+    if x % 2 == 1:
+     print(x)
+    x += 1
