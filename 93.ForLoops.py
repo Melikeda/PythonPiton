1e5064879c7b53cb2ebde76ebf4ef0ae08cf86f6
Melike Külahcı <m.edakulahci@gmail.com>
2024-06-29 11:58:40 +0300

    Add files via upload

---
+++ 93.ForLoops.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5]
+
+for x in numbers:
+    print(x)
+
+for num in numbers:
+    print("Hello")    
+
+
+names = ["melike " "eda"]
+
+for name in names:
+    print( f"My name is: {name}")  #f string
+
+
+tuple = [(1,2), (2,3), (3,4)]
+
+for t in tuple:
+    print(t)
