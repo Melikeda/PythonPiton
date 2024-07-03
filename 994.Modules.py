9a7a70987c5a340718e66bb478b80669052c7649
Melike Külahcı <m.edakulahci@gmail.com>
2024-07-03 16:29:39 +0300

    Add files via upload

---
+++ 994.Modules.py
@@ -0,0 +1,22 @@
+# way1
+
+import math
+
+#value = dir(math)
+#value = help(math)
+#value = help(math.factorial)
+
+value = math.sqrt(49)
+value = math.factorial(5)
+
+print(value)
+
+
+# way2
+
+from math import *
+
+value = factorial(6)
+
+print(value)
+
