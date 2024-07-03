da1363ce606c7c5bdd1b093a3a825c9885557d3a
Melike Külahcı <m.edakulahci@gmail.com>
2024-07-03 19:53:15 +0300

    Add files via upload

---
+++ 996.Files.py
@@ -0,0 +1,8 @@
+# Open() function is used to open and create files
+
+# "w": (write) writing mode. Creates the file at the location
+
+file = open("newfile.txt", "w")
+file.write("QUEEN")
+file.close()
+
