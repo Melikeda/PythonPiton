ced2844d23bc466b6eebfc9b3fe696a62933e8b4
Melike Külahcı <m.edakulahci@gmail.com>
2024-07-09 14:48:05 +0300

    Add files via upload

---
+++ 9991.Matplotlib2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1,2,3,4]
+y = [1,4,9,16]
+
+plt.plot(x,y, "--g")
+plt.axis([0,5,0,20])
+
+plt.title("Chart Title")
+plt.xlabel("x Label")
+plt.ylabel("y Label")
+
+plt.show()
